84810435ec4a3099841df5bc3bac8f6043c15fcd
Jasper Loo Zhu Hang <zhuhang.loo@uberfusion.com>
2018-04-04 18:57:04 +0800

    fastBuildBAT v1.5.2 fix - Fix END command not killing JBOSS CMD if frozen by user text selection

--- source/end.bat
+++ source/end.bat
@@ -1,3 +1,4 @@
 @call .preLoad.bat
 @echo [33mKilling on-going running JBOSS servers...[0m
+@taskkill /IM cmd.exe /FI "WINDOWTITLE eq Select JBOSS_MyClear*"
 @taskkill /IM cmd.exe /FI "WINDOWTITLE eq JBOSS_MyClear*"
