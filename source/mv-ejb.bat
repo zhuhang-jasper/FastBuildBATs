081837184fc04fb1a498b59dbe1ac204d897c33a
Jasper Loo Zhu Hang <zhuhang.loo@uberfusion.com>
2018-04-18 11:04:45 +0800

    hot fix fastBuildBAT - Avoid EJB client jar export target folder not found

--- source/mv-ejb.bat
+++ source/mv-ejb.bat
@@ -3,4 +3,5 @@
 @del /S /Q "%fpx_ejb_client_dir%\fpx-admin-ejb-client*"
 @pushd "%fpx_ejb_jar_dir%"
 @echo [33mExporting EJB client JAR file to Webview libs folder...[0m
+@if not exist "%fpx_ejb_client_dir%" mkdir "%fpx_ejb_client_dir%"
 @copy fpx-admin-ejb-client* "%fpx_ejb_client_dir%\" /Y & @popd
