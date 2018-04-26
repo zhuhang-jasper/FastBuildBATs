a7c32d108b87791006ebe8054d873c436484c691
Jasper Loo Zhu Hang <zhuhang.loo@uberfusion.com>
2018-04-26 09:40:23 +0800

    Fix tool fastbuildBAT - ROOT command - not toggling to project path

--- source/root.bat
+++ source/root.bat
@@ -1,3 +1,3 @@
 @call .preLoad.bat
 
-@if "%~1"=="e" (explorer %root%) else ( @if "%cd%\"=="%root%" (cd %fpx_root%) else (cd %root%) )
+@if "%~1"=="e" (explorer %root%) else ( @if "%cd%\."=="%root%" (cd %fpx_root%) else (cd %root%) )
