2a756b66b63a389363178098ae367d43280a8d89
Jasper Loo Zhu Hang <zhuhang.loo@uberfusion.com>
2018-06-07 11:52:26 +0800

    FastBuildBAT fix buildRelease accident remove source files

--- source/br-ejb.bat
+++ source/br-ejb.bat
@@ -1,6 +1,10 @@
 @call .preLoad.bat
+
+@if not exist %fpx_ear_dir% (goto BUILD)
 @pushd "%fpx_ear_dir%"
 @for /D %%f in (fpx-admin-ejb-ear*) do @echo [33mRemoving old EAR ^>^> %%f [0m & @rmdir %%f /S /Q
+
+:BUILD
 @echo [33mGRADLE ^> %fpx_ejb_dir%\%gradle_cmd_buildrelease%%Penv% [0m
 @cd %fpx_ejb_dir%
 @call %gradle_cmd_buildrelease%%Penv%
--- source/br.bat
+++ source/br.bat
@@ -1,6 +1,10 @@
 @call .preLoad.bat
+
+@if not exist %fpx_war_dir% (goto BUILD)
 @pushd "%fpx_war_dir%"
 @for /D %%f in (fpx-admin-webview*) do @echo [33mRemoving old WAR ^>^> %%f [0m & @rmdir %%f /S /Q
+
+:BUILD
 @echo [33mGRADLE ^> %fpx_admin_dir%\%gradle_cmd_buildrelease%%Penv% [0m
 @cd %fpx_admin_dir%
 @call %gradle_cmd_buildrelease%%Penv%
