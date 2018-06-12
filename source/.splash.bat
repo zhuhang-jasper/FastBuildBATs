b7507953933ab1de6f6fddeeac85deb4e68ba06a
Jasper Loo Zhu Hang <zhuhang.loo@uberfusion.com>
2018-06-12 13:11:04 +0800

    FastBuildBATs Update ASCII art

--- source/.splash.bat
+++ source/.splash.bat
@@ -10,29 +10,26 @@ if "%theme%"=="" set "theme=Green"
 :: Apply Theme. options: Green, Holland
 if "%theme%"=="Green" (set "col1=%green%" & set "col2=%green%" & set "col3=%green%" & set "col4=%yellow%" & set "thgap=  ")
 if "%theme%"=="Holland" (set "col1=%red%" & set "col2=%white%" & set "col3=%cyan%" & set "col4=%white%" & set "mark=FIFA")
+call :VERTITLE
 if "%~1"=="/dashboard" (goto DASHBOARD) else (goto SPLASH)
 
 :DASHBOARD
 echo  %col2%--------------------------------------------------------------------%off%
 call :BIGTITLE
-echo %col4%                                                _  ____  __ 
-echo              Fast Build + Deploy Commands  __ _/ ^|^|__  ^|/ / 
-echo                Gradle, JBOSS, GIT, 7zip    \ V / ^|_ / // _ \
-echo                   by: zhuhang.jasper        \_/^|_^(_^)_^(_^)___/ %off%
+echo                                           %col4%%vArt1%
+echo              Fast Build + Deploy Commands %vArt2%
+echo                Gradle, JBOSS, GIT, 7zip   %vArt3%
+echo                   by: zhuhang.jasper      %vArt4%%off%
 goto END
 
 :SPLASH
 @cls
 echo. & echo. & echo. & echo. & echo.
 call :BIGTITLE
-echo                       %col4%       _    ____    __    %off%
-echo                       %col4% __ __/ ^|  ^|__  ^|  / / %off%
-echo                       %col4% \ V /^| ^| _  / /_ / _ \ %off%
-echo                       %col4%  \_/ ^|_^|(_)/_/(_)\___/ %off%
-::echo                        [32m      _    ____    __ [0m
-::echo                        [32m__ _ [32m/ ^|  ^|__  ^|  / / [0m
-::echo                        [32m\ V /[32m^| ^| [32m_[32m  / /[32m_ [32m/ _ \ [0m
-::echo                        [32m \_/ [32m^|_^|[32m^(_^)[32m/_/[32m^(_^)[32m\___/ [0m
+echo                        %col4% %vArt1% %off%
+echo                        %col4% %vArt2% %off%
+echo                        %col4% %vArt3% %off%
+echo                        %col4% %vArt4% %off%
 echo. & echo. 
 echo                     %green%Proudly presented by Zhu Hang
 echo                             - UberFusion -%off%
@@ -46,6 +43,15 @@ echo %thgap%         %col1% ___        _   %col2%___      _ _    _ %col3%___   _
 echo %thgap%         %col1%^| __^|_ _ __^| ^|_%col2%^| _ )_  _(_) ^|__^| %col3%^| _ ) /_\_   _^|__ %off%
 echo %thgap%         %col1%^| _/ _` (_-^<  _%col2%^| _ \ ^|^| ^| ^| / _` %col3%^| _ \/ _ \^| ^|(_-^< %off%
 echo %thgap%         %col1%^|_^|\__,_/__/\__%col2%^|___/\_,_^|_^|_\__,_%col3%^|___/_/ \_\_^|/__/ %white%%mark%%off%
+goto :EOF
+
+::@called
+:VERTITLE
+set "vArt1=" & set "vArt2=" & set "vArt3=" & set "vArt4="
+set "vArt1=%vArt1%      _  ____ ____ "
+set "vArt2=%vArt2% __ _/ ^|^|__  ^|__  ^|"
+set "vArt3=%vArt3% \ V / ^|_ / /  / / "
+set "vArt4=%vArt4%  \_/^|_(_)_(_)/_/  "
 goto :EOF
 
 :END
